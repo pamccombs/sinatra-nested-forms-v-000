d4e515a4afa7766e0a8e5238198ba0e29a8eb1cc
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-23 14:51:29 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize(args)
+    @name=args[:name]
+    @weight=args[:weight]
+    @height=args[:height]
+    @@all << self
+  end
+  
+  def self.all
+  @@all
+  end
+  
+  def self.clear
+    @@all.clear
+  end
 end
